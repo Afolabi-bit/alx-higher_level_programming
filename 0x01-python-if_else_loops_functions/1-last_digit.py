84b6c8a06b71835f76950356db08196b3df35ded
afolabi <maverickoluwatomisin@gmail.com>
2022-11-29 19:54:57 +0100

    aplhabet

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+	number *= -1
+	if number 
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for c in (chr(i) for i in range(97, 123)):
+	print(c, end='')
