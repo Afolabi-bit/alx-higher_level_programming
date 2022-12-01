40e9a3d868d48476d6e107042c21baf9ea71fd2e
afolabi <maverickoluwatomisin@gmail.com>
2022-12-01 21:38:20 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
     if length == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif length == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(length))
 
