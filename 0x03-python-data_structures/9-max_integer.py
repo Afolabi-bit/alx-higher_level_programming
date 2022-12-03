a8cc15111318eeea8bd63c4a646f721b4b337cfe
afolabi <maverickoluwatomisin@gmail.com>
2022-12-03 20:33:12 +0100

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
+    if len(my_list) == 0:
         return None
     a = 0
     for i in my_list:
         if i >= a:
             a = i
-        else:
-            continue
     return a
