1222931710d4df2b663fc9134ff61f0308fb57cd
afolabi <maverickoluwatomisin@gmail.com>
2022-12-07 20:34:47 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if not my_list:
-        return None
+        return my_list
     return list(i if i != search else replace for i in my_list)
