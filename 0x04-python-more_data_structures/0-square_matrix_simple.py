9188ed0bf585a295628de16ade0634a8d8816911
afolabi <maverickoluwatomisin@gmail.com>
2022-12-07 20:16:13 +0100

    initial

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
     for row in matrix:
-        f.append(list(map(lambda x: x**2,row)))
-    return f
+        f.append(list(map(lambda x: x**2, row)))
+    return (f)
