cb159d75ffceac44b85daa6c8f5bf1e5d980ff80
afolabi <maverickoluwatomisin@gmail.com>
2022-12-07 20:14:34 +0100

    initial

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    f = []
+    if not matrix:
+        return None
+    for row in matrix:
+        f.append(list(map(lambda x: x**2,row)))
+    return f
