1bfacac2573ae8311ad09fd76f48976ccd1dee7d
afolabi <maverickoluwatomisin@gmail.com>
2022-12-08 23:00:07 +0100

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     import sys
 
-    nargs = len(sys.argv) - 1
-    if nargs != 3:
+    args_count = len(sys.argv) - 1
+    if args_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
