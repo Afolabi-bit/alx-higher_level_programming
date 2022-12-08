cdf1ed1c230b3e04736052caa414419b1e881760
afolabi <maverickoluwatomisin@gmail.com>
2022-12-08 22:12:30 +0100

    insert insorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into
+ * a sorted singly linked list
+ * @head: pointer to list head
+ * @number: number to be inserted
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = number;
+
+	temp = *head;
+	while (temp)
+	{
+		if (temp->next->n > new->n)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		temp = temp->next;
+	}
+	free(temp);
+	return (NULL);
+}
