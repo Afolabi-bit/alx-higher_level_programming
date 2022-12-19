8a83409f4c7dc780f8fbe2dffe5836d99f1916c3
afolabi <maverickoluwatomisin@gmail.com>
2022-12-19 21:50:08 +0100

    except Exception

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
+    else:
+        return (True)
