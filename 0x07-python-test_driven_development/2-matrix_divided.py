0e1375bd5b4c356034b894982b5a416f075121aa
afolabi <maverickoluwatomisin@gmail.com>
2023-01-05 23:03:15 +0100

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,10 +4,11 @@
 matrix division module
 """
 
+
 def matrix_divided(matrix, div):
     """
-	matrix: must be a list of lists of int or floats
-	div: number above zero
+        matrix: must be a list of lists of int or floats
+        div: number above zero
     """
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -15,20 +16,21 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     if type(matrix) is not list:
-         raise TypeError("matrix must be a matrix "
-			 "(list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     size = 0
     for row in matrix:
         if type(row) is not list:
             raise TypeError("matrix must be a matrix "
-			    "(list of lists) of integers/floats")
+                            "(list of lists) of integers/floats")
         if size == 0:
             size = len(row)
         elif size != len(row):
             raise TypeError("Each row of the matrix "
-			    "must have the same size")
+                            "must have the same size")
         for num in row:
             if not isinstance(num, (int, float)):
                 raise TypeError("matrix must be a matrix "
-				"(list of lists) of integers/floats")
-    return list(map(lambda a: list(map(lambda b: round(b / div, 2), a)) , matrix.copy()))
+                                "(list of lists) of integers/floats")
+    m = matrix.copy()
+    return list(map(lambda a: list(map(lambda b: round(b / div, 2), a)), m))
