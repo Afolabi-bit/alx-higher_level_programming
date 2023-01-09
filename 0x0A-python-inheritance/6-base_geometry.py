378ac4e5953f219234926a88e57adbd1d48f376b
afolabi <maverickoluwatomisin@gmail.com>
2023-01-09 23:10:17 +0100

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """class"""
+    def area(self):
+        raise Exception("area() is not implemented")
