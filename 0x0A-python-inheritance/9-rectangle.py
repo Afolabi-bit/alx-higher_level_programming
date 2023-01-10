980ae85365bd35e14628d11150ff2b3dd34b857f
afolabi <maverickoluwatomisin@gmail.com>
2023-01-10 21:27:24 +0100

    9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a class Rectangle that
+inherits from BaseGeometry (7-base_geometry.py)
+and is based on 8-rectangle.py
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rect class """
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Returns the area of a rectangle """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """Str representation of rect """
+        return "Rectangle {:d}/{:d}".format(self.__width, self.__height)
