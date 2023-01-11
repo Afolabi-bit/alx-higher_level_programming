4288f9156f972d0ff43b2b904fcaf571d6fbaa05
afolabi <maverickoluwatomisin@gmail.com>
2023-01-11 21:39:01 +0100

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -14,5 +14,5 @@ def write_file(filename="", text=""):
     Return:
         The number of characters written.
     """
-    with open(filename, w, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
