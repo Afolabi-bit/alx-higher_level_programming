e138fa4f4a71e8475ef45f239310575ffb576a0d
afolabi <maverickoluwatomisin@gmail.com>
2023-01-11 21:14:57 +0100

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ text file (UTF8) and prints it to stdout
 
 
 def read_file(filename=""):
+    """ Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
