59d5b61e3c4994a9439cf046ff58e104067c1a96
afolabi <maverickoluwatomisin@gmail.com>
2023-01-12 21:51:17 +0100

    Student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,13 +8,13 @@ class Student():
     """ Student """
     def __init__(self, first_name, last_name, age):
         """ Initialize a new Student
-	Args:
+        Args:
             first_name (str): The first name of Student
             last_name (str): The last name of Student
             age (int): age of Student
         """
         self.first_name = first_name
-	self.last_name = lst_name
+        self.last_name = lst_name
         self.age = age
 
     def to_json(self):
