50913e555753526ce74b8cf88d309acba574ba8c
afolabi <maverickoluwatomisin@gmail.com>
2023-01-12 21:05:17 +0100

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,8 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+			  __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_item():
