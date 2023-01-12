61d904764a975f6ada932f846bcee9860bebcbd8
afolabi <maverickoluwatomisin@gmail.com>
2023-01-12 20:11:30 +0100

    loads from json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     Args:
         filename: name of file
     """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
