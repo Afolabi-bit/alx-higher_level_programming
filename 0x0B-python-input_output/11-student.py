6a6130d1a94b46f45780299c49119c9e2133f28f
afolabi <maverickoluwatomisin@gmail.com>
2023-01-12 22:42:42 +0100

    e dey carry me go

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Defines a class Student
+"""
+
+
+class Student():
+    """ Student """
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a new Student
+        Args:
+            first_name (str): The first name of Student
+            last_name (str): The last name of Student
+            age (int): age of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dict representation of Student
+            If attrs is a list of strings,
+            represents only those attributes included in the list.
+            Args:
+                attrs (list): attribute
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+        Args:
+            json (dict): The key/value pairs to replace attributes with
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
