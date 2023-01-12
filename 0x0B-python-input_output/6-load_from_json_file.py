d0e047895bc2cad37a218b1c5696732c0c80a5f6
afolabi <maverickoluwatomisin@gmail.com>
2023-01-12 20:08:38 +0100

    load from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a function that creates an
+Object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Args:
+        filename: name of file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return json.load(f)
