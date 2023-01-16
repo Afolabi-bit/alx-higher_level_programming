9276dc36fd1cfc20b358690ddcc56dbfcdf21c60
afolabi <maverickoluwatomisin@gmail.com>
2023-01-16 23:04:01 +0100

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines the base class for all other
+classes in this package
+"""
+
+
+class Base():
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Class constructor initialization """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
