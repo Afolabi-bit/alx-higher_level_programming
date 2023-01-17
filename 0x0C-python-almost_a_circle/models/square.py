0252f635ecdf144fcdde33b2c4eab81b13c997c6
afolabi <maverickoluwatomisin@gmail.com>
2023-01-17 20:10:32 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,13 @@ class Square(Rectangle):
 
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
+
+    @property
+    def size(self):
+        """ size getter """
+        return super().width
+
+    @size.setter
+    def size(self, width):
+        """ size setter """
+        self.width = width
