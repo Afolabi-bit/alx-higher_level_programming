281ebdd6f03e704bd24e8e1d99b33bb37b8962e4
afolabi <maverickoluwatomisin@gmail.com>
2023-01-17 23:41:17 +0100

    xyz

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,20 @@ class Base():
         if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """
+            Saves obj as json
+        """
+        filename = "{}.json".format(cls)
+        obj_list = []
+
+        if list_objs is None:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                obj_list.append(list_objs[i].to_dictionary())
+
+            lists = cls.to_json_string(obj_list)
+            with open(filename, "w") as f:
+                f.write(lists)
