636ec4577ac70c6b111789d600c2b915a7e0e33c
afolabi <maverickoluwatomisin@gmail.com>
2023-01-17 21:55:31 +0100

    json representation of object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
             Returns the JSON str of list_dictionary
