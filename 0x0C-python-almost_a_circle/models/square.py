d2c139f9f6d15d972a67f9d519739bfd373f1e5e
afolabi <maverickoluwatomisin@gmail.com>
2023-01-17 19:52:23 +0100

    Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class
+which inherits from Rectangle(rectangle.py)
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class that defines a square """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Instantiation of variables """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
