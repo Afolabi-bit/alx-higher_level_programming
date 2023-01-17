37ee50bda4b9bccbc0d2e82eebbdb99e358bf3bf
afolabi <maverickoluwatomisin@gmail.com>
2023-01-17 17:07:01 +0100

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,27 +21,34 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, w):
         self.width = w
 
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, h):
         self.height = h
 
+
     @property
     def x(self):
         return self.__x
+
     @x.setter
     def x(self, x):
         self.__x = x
 
+
     @property
     def y(self):
         return self.__y
+
     @y.setter
     def y(self, y):
         self.__y = y
