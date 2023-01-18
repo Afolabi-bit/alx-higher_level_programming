ccc6cf435309bbc017951d744877a62ca714c469
afolabi <maverickoluwatomisin@gmail.com>
2023-01-18 20:40:04 +0100

    create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,6 +57,7 @@ class Base():
             return []
         return json.loads(json_string)
 
+    @classmethod
     def create(cls, **dictionary):
         """
             returns an instance with all
