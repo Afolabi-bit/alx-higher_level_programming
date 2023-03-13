87cc2371088d113a3745a72e6dd2339a880078df
afolabi <maverickoluwatomisin@gmail.com>
2023-03-14 00:04:24 +0100

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const ints = args.map(x => parseInt(x)).filter(num => Number.isInteger(num));
+
+if (ints.length === 0) {
+  console.log(0);
+} else if (ints.length === 1) {
+  console.log(1);
+} else {
+  const secondLargest = ints.sort((a, b) => b - a)[1];
+  console.log(secondLargest);
+}
