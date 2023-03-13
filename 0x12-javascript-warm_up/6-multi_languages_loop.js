c96622780fb2e3e867f575c7bc96090b244f2104
afolabi <maverickoluwatomisin@gmail.com>
2023-03-13 22:54:58 +0100

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (arr[i]) {
   console.log(arr[i]);
