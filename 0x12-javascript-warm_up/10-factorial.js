7c0a5e72cff86035fb1c47101fcaee71cff4b159
afolabi <maverickoluwatomisin@gmail.com>
2023-03-13 23:44:52 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial(num){
+  if (!parseInt(num))
+    return (1);
+
+  parseInt(num);
+  if (num === 1)
+    return (1);
+
+  return (num * factorial(num - 1));
+}
+
+console.log(factorial(process.argv[2]));
