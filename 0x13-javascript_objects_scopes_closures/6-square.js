eeb727ee22aae7a300e2d90659515c4412b448e5
afolabi <maverickoluwatomisin@gmail.com>
2023-03-14 22:32:55 +0100

    using c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && typeof (w) === 'number' && typeof (h) === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const PrevSquare = require('./5-square');
 
-  print () {
-    for (let j = 0; j < this.height; j++) {
-      let ch = '';
-      for (let i = 0; i < this.width; i++) {
-        ch += 'X';
-      }
-      console.log(ch);
-    }
-  }
-
-  rotate () {
-    const w = this.width;
-    this.width = this.height;
-    this.height = w;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends PrevSquare {
   constructor (size) {
     super(size, size);
   }
