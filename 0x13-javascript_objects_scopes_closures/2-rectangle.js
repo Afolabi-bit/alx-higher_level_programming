8af6ec8a8a03430d835a970cefc0f8dc10c906f4
afolabi <maverickoluwatomisin@gmail.com>
2023-03-14 21:19:07 +0100

    initialization

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || !w || !h || h <= 0)
-    {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (w > 0 && typeof(w) === 'number' && typeof(h) === 'number' && h > 0){
       this.width = w;
       this.height = h;
     }
