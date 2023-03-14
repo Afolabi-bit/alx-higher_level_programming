9e7acf53eb7cf97ab6489550ef43d1ac21c00e64
afolabi <maverickoluwatomisin@gmail.com>
2023-03-14 21:38:49 +0100

    methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && typeof (w) === 'number' && typeof (h) === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      let ch = '';
+      for (let i = 0; i < this.width; i++) {
+        ch += 'X';
+      }
+      console.log(ch);
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
