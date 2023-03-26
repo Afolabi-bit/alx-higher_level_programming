462dec3aa988cfc2cffab2dbd61af18ad14e585b
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-03-26 10:47:41 +0000

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,7 +3,6 @@
 This script lists all states with a name starting with N
 from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
 from sys import argv
 
@@ -18,8 +17,10 @@ if __name__ == "__main__":
     cursor.execute("SELECT c.id, c.name, s.name FROM cities as c\
                     inner join states as s\
                     ON c.state_id = s.id\
-                    WHERE s.name = %s", (sys.argv[4],))
+                    WHERE s.name = %s", (argv[4],))
+
     cities = cursor.fetchall()
+
     print(", ".join([city[1] for city in cities])
     cursor.close()
     db.close()
