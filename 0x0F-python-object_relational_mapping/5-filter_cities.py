f47007b56a5bb5d46321ad58bc15ae62d90a3b7f
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-03-26 09:47:02 +0000

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT c.id, c.name, s.%(name)s FROM cities as c\
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities as c\
                     inner join states as s\
-                    ON c.state_id = s.id", {'name': argv[4]})
+                    ON c.state_id = s.id\
+                    WHERE s.name = %s", (sys.argv[4],))
     cities = cursor.fetchall()
     for c in cities:
         print(c)
