3d93c803b9e84fb9a613ba8b19659452db5bfdc9
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-03-26 17:22:56 +0000

    base class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@
 """
 
 from sqlalchemy import Column, Integer, String
-form sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
