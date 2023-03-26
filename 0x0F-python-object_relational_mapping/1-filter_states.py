e56207bec75dfd972bedc0f6bc93ddc74d888e27
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-03-26 01:22:57 +0000

    selects names with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name Like 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                   ORDER BY states.id ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
