1254da59eb6719000290780d8046c24304797d6a
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-03-26 02:52:49 +0000

    cities in states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a name starting with N
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+        Access db and print states
+    """
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities as c inner join \
+                   states as s ON states_id = s.id")
+    cities = cursor.fetchall()
+    for c in cities:
+        print(c)
+    cursor.close()
+    db.close()
