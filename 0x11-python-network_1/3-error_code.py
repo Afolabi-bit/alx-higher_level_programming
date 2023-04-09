eb0c8b0593479c4f56efa6bde64467cf2e8464f7
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-04-09 19:47:07 +0000

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+   request to a URL and displays the response body
+   Handles HTTP errors.
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
