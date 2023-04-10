95655f2fa0f575dba0ac4db0d9b451bc39b84281
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-04-10 05:45:48 +0000

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Sends a request to a passed URL and displays the response body.
+    For HTTP errors.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
